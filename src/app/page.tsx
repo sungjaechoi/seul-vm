bd26442480307de8a0819b4a080edc4995543f34
최성재 <sungjaechoi39@gmail.com>
2024-05-19 20:59:02 +0900

    visaulSection img추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,12 @@ export default async function page() {
     description:
       '인터넷의 시각 자료 출처입니다. 모든 지역에 있는 크리에이터들의 지원을 받습니다.',
   }
-  const images = await getImages('random')
+  const images = (await getImages('random')) || []
+  const firsImageSrc = images[0] ? images[0].urls.full : ''
+
   return (
     <>
-      <VisualSection v={v} />
+      <VisualSection v={v} src={firsImageSrc} />
       <section className={style.image_section}>
         <h3 className="blind">이미지 리스트</h3>
         <div className={style.images_section_inner}>
