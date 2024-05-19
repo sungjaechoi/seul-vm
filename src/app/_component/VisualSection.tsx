a5e833c4a5dfb1e82f8f7057ad73abbe145d7858
최성재 <sungjaechoi39@gmail.com>
2024-05-19 20:59:02 +0900

    visaulSection img추가

--- src/app/_component/VisualSection.tsx
+++ src/app/_component/VisualSection.tsx
@@ -1,17 +1,26 @@
-import { TImage } from '@/model/Image'
+import { Image } from '@/model/Image'
 import style from './visualSection.module.css'
 
 type Props = {
   v: { title: string; description: string }
+  src: string
 }
 
-export default function VisualSection({ v: { title, description } }: Props) {
+export default function VisualSection({
+  v: { title, description },
+  src,
+}: Props) {
   return (
     <section className={style.visual_section}>
       <div className={style.visual_section_inner}>
         <h2 className={style.section_title}>{title}</h2>
-        <span className={style.description}>{description}</span>
+        <span className={style.section_description}>{description}</span>
       </div>
+      {src && (
+        <div className={style.visual_img_area}>
+          <img src={src} alt="" />
+        </div>
+      )}
     </section>
   )
 }
--- src/app/gallery/[name]/page.tsx
+++ src/app/gallery/[name]/page.tsx
@@ -15,11 +15,12 @@ export default async function Page({ params }: Props) {
   //! Notion - "as Key of"정리
   const nameV = params.name as keyof typeSectionInfo
   const v = sectionInfo[nameV]
-  const images = await getImages(nameV)
+  const images = (await getImages(nameV)) || []
+  const firsImageSrc = images[0] ? images[0].urls.full : ''
 
   return (
     <div>
-      <VisualSection v={v} />
+      <VisualSection v={v} src={firsImageSrc} />
       <section className={style.image_section}>
         <h3 className="blind">이미지 리스트</h3>
         <div className={style.images_section_inner}>
