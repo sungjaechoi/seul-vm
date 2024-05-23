affbf6fd007f233359f8e2765df90135627c4105
최성재 <sungjaechoi39@gmail.com>
2024-05-23 10:13:20 +0900

    상세페이지 구성

--- src/app/gallery/[name]/photo/[photoId]/page.tsx
+++ src/app/gallery/[name]/photo/[photoId]/page.tsx
@@ -1,21 +1,14 @@
-// import VisualSection from '@/app/_component/VisualSection'
-// import { getImages } from '@/app/_lib/getImages'
-// import { sectionInfo, typeSectionInfo } from '@/model/SectionInfo'
+type Props = {
+  params: {
+    name: string
+    photoId: string
+  }
+}
+export default function page({ params }: Props) {
+  params.name
+  params.photoId
+  console.log('pathName', params.name)
+  console.log('photo`Id', params.photoId)
 
-// type Props = {
-//   params: {
-//     name: string
-//     photoId: string
-//   }
-// }
-// export default function page({ params }: Props) {
-//   params.name
-//   params.photoId
-//   console.log('pathName', params.name)
-//   console.log('photo`Id', params.photoId)
-
-//   return <div></div>
-// }
-export default function page() {
-  return <div></div>
+  return <div>포토 상세페이지</div>
 }
