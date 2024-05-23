794d17cc6a44a4828c25c7f1d179659558086c15
최성재 <sungjaechoi39@gmail.com>
2024-05-23 10:09:31 +0900

    패러럴라우트트 폴더 구성 변경점

--- src/app/gallery/[name]/photo/[photoId]/page.tsx
+++ src/app/gallery/[name]/photo/[photoId]/page.tsx
@@ -1,15 +1,21 @@
-import { LayoutRouter } from 'next/dist/server/app-render/entry-base'
+// import VisualSection from '@/app/_component/VisualSection'
+// import { getImages } from '@/app/_lib/getImages'
+// import { sectionInfo, typeSectionInfo } from '@/model/SectionInfo'
 
-type Props = {
-  params: {
-    name: string
-    photoId: string
-  }
-}
-export default function page({ params }: Props) {
-  params.name
-  params.photoId
-  console.log(params.name)
-  console.log(params.photoId)
-  return <LayoutRouter />
+// type Props = {
+//   params: {
+//     name: string
+//     photoId: string
+//   }
+// }
+// export default function page({ params }: Props) {
+//   params.name
+//   params.photoId
+//   console.log('pathName', params.name)
+//   console.log('photo`Id', params.photoId)
+
+//   return <div></div>
+// }
+export default function page() {
+  return <div></div>
 }
