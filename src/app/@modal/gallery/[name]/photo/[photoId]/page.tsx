c71734271d2786211ae0bf745a50ce8550b37993
최성재 <sungjaechoi39@gmail.com>
2024-05-23 12:04:03 +0900

    라우팅 그룹

---
+++ src/app/@modal/gallery/[name]/photo/[photoId]/page.tsx
@@ -0,0 +1,7 @@
+export default function page() {
+  return (
+    <div style={{ width: '100%', height: '100%', background: 'red' }}>
+      패러럴 라우트 페이지
+    </div>
+  )
+}
--- src/app/gallery/[name]/photo/[photoId]/page.tsx
+++ src/app/gallery/[name]/photo/[photoId]/page.tsx
@@ -9,6 +9,4 @@ export default function page({ params }: Props) {
   params.photoId
   console.log('pathName', params.name)
   console.log('photo`Id', params.photoId)
-
-  return <div>포토 상세페이지</div>
 }
