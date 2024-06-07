ab52075c5d27bd35b762153e6e0458b4dd28bf9b
최성재 <sungjaechoi39@gmail.com>
2024-06-07 18:31:47 +0900

    searchform과 searchRoute 구성

---
+++ src/app/_lib/getSearchResult.ts
@@ -0,0 +1,22 @@
+export default async function getSearchResult(searchKeyword: string) {
+  try {
+    const apiKey = process.env.NEXT_PUBLIC_API_KEY
+    if (!apiKey) {
+      throw new Error('API KEY가 잘못되었습니다.')
+    }
+
+    const response = await fetch(
+      `https://api.unsplash.com/search/photos?client_id=${apiKey}&per_page=30&query=${searchKeyword}`,
+    )
+
+    if (!response.ok) {
+      throw new Error(`Http Error status: ${response.status}`)
+    }
+    const searchData = await response.json()
+    const imageArray = searchData.results
+
+    return imageArray
+  } catch (error) {
+    console.error('fetchError :', error)
+  }
+}
