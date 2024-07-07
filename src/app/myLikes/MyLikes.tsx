563ba8896ff513657e4b364814ac27483b7298e8
최성재 <sungjaechoi39@gmail.com>
2024-07-07 19:08:23 +0900

    좋아요가 0일떄 알림페이지, 좋아요 수정

--- src/app/myLikes/MyLikes.tsx
+++ src/app/myLikes/MyLikes.tsx
@@ -1,12 +1,24 @@
 'use client'
-
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { likesContext } from '../_component/LikesProvider'
 import ImgList from '@/app/_component/ImgList'
+import style from './myLikes.module.css'
 
 export default function MyLikes() {
   const { likes } = useContext(likesContext)
   const images = likes.map((like) => like.img)
+  const [likesImg, setLikesImg] = useState(images)
 
-  return <ImgList images={images} query="" />
+  return likesImg.length === 0 ? (
+    <div className={style.not_mylikes_area}>
+      <h2 className="blind">좋아요를 표시한 이미지 항목</h2>
+      <div className={style.img_box}></div>
+      <span>좋아요를 표시한 사진이 없습니다.</span>
+    </div>
+  ) : (
+    <>
+      <h2 className="blind">좋아요를 표시한 이미지 항목</h2>
+      <ImgList images={likesImg} query="" />
+    </>
+  )
 }
