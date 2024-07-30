97534956cccf1305164d89f3cfabda1ca68a189f
최성재 <sungjaechoi39@gmail.com>
2024-07-31 04:32:05 +0900

    Router cache ravalidate : 0

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/gallery/:name',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 's-maxage=1, stale-while-revalidate=59',
-          },
-        ],
-      },
-    ]
-  },
   reactStrictMode: false,
   images: {
     remotePatterns: [
